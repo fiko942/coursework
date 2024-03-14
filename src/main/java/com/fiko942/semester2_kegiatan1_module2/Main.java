e4150966222f64d37da0156948bb95520fd772b5
fiko942 <tobellord@gmail.com>
2024-03-14 21:38:09 +0700

    completing main menu of program

--- src/main/java/com/fiko942/semester2_kegiatan1_module2/Main.java
+++ src/main/java/com/fiko942/semester2_kegiatan1_module2/Main.java
@@ -1,7 +1,60 @@
 package com.fiko942.semester2_kegiatan1_module2;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static Student student = new Student();
+    private static Admin admin = new Admin();
+    private static ArrayList<Book> books = new ArrayList<>();
+
+    /**
+     * Initializes the list of books with predefined book entries.
+     *
+     */
+    private static void initBook() {
+        books.add(new Book(1, 1, "Wiji Fiko Teren", "Sang Pemimpi"));
+        books.add(new Book(2, 2, "Wiji Fiko Teren", "Samlekommm"));
+        books.add(new Book(3, 3, "Wiji Fiko Teren", "Tak bukan bukan"));
+    }
+
+
+    /**
+     * Menu function that displays the library system menu, takes user input, and returns the chosen option.
+     *
+     * @return         	the user's choice as an integer
+     */
+    private static int Menu() {
+        System.out.println("=== Library System ===");
+        ArrayList<String> menus = new ArrayList<>(List.of(new String[]{
+                "Login as Admin",
+                "Login as Student",
+                "Exit"
+        }));
+        menus.forEach(s -> {
+            int i = menus.indexOf(s) + 1;
+            System.out.printf("%d. %s\n", i, s);
+        });
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose option (1-3): ");
+        int choice = scanner.nextInt();
+        if(choice == 3) {
+            System.exit(0);
+            return -1;
+        } else {
+            return choice;
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        initBook();
+        while(true) {
+            int mainMenuChoiced = Menu();
+            System.out.println("mainMenuChoiced: " + mainMenuChoiced);
+        }
     }
 }
--- src/main/java/com/fiko942/semester2_kegiatan1_module2/Student.java
+++ src/main/java/com/fiko942/semester2_kegiatan1_module2/Student.java
@@ -1,4 +1,15 @@
 package com.fiko942.semester2_kegiatan1_module2;
 
 public class Student {
+    String name;
+    String faculty;
+    String programStudy;
+
+    public static void logout() {
+
+    }
+
+    public static void displayBooks() {
+
+    }
 }
